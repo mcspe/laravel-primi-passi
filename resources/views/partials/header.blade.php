0b2bfe44dc0f939ebd500f53f26d32ff2b70117c
mcspe <mcspezzaferro@gmail.com>
2023-06-08 18:18:42 +0200

    [first commit] add repo

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <nav>
+        <ul>
+            <li>
+                <a href="{{route('home')}}">HOME</a>
+            </li>
+            <li>
+                <a href="{{route('contacts')}}">CONTATTI</a>
+            </li>
+        </ul>
+    </nav>
+</header>
